a0de1badd854c72f97616273baf4c5a6c6f4b6e8
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 18:34:00 +0530

    mecha-pwm-working-v2.1

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
 extern crate sysfs_pwm;
+use std::env;
 use sysfs_pwm::{Pwm, Result};
 
-// Specify the PWM chip and pin for the servo motor
-const SERVO_PWM_CHIP: u32 = 0;   // Update with the actual chip number
-const SERVO_PWM_PIN: u32 = 0;    // Update with the actual pin number
+// PIN: EHRPWM0A (P9_22)
+const SERVO_PWM_CHIP: u32 = 0;
+const SERVO_PWM_PIN: u32 = 0;
 
-fn servo_increase_to_max(pwm: &Pwm, duration_ms: u32, update_period_ms: u32) -> Result<()> {
-    let step: f32 = 1.0 / (duration_ms as f32 / update_period_ms as f32);
-    let mut duty_cycle = 0.0;
+fn move_servo(pwm: &Pwm, min_position: f32, max_position: f32) -> Result<()> {
     let period_ns: u32 = pwm.get_period_ns()?;
-    while duty_cycle < 1.0 {
-        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
-        duty_cycle += step;
-    }
-    pwm.set_duty_cycle_ns(period_ns)
-}
+    let min_duty_cycle_ns = (min_position * period_ns as f32) as u32;
+    let max_duty_cycle_ns = (max_position * period_ns as f32) as u32;
 
-fn servo_decrease_to_minimum(pwm: &Pwm, duration_ms: u32, update_period_ms: u32) -> Result<()> {
-    let step: f32 = 1.0 / (duration_ms as f32 / update_period_ms as f32);
-    let mut duty_cycle = 1.0;
-    let period_ns: u32 = pwm.get_period_ns()?;
-    while duty_cycle > 0.0 {
-        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
-        duty_cycle -= step;
-    }
-    pwm.set_duty_cycle_ns(0)
+    pwm.set_duty_cycle_ns(min_duty_cycle_ns)?;
+    pwm.enable(true)?;
+
+    // Move to the maximum position
+    pwm.set_duty_cycle_ns(max_duty_cycle_ns)?;
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    // Move to the minimum position
+    pwm.set_duty_cycle_ns(min_duty_cycle_ns)?;
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    pwm.enable(false)?;
+    Ok(())
 }
 
 fn main() {
-    let pwm = Pwm::new(SERVO_PWM_CHIP, SERVO_PWM_PIN).unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: {} <min_position> <max_position>", args[0]);
+        return;
+    }
+
+    let min_position: f32 = args[1].parse().unwrap_or(0.0);
+    let max_position: f32 = args[2].parse().unwrap_or(1.0);
+
+    let pwm = Pwm::new(SERVO_PWM_CHIP, SERVO_PWM_PIN).unwrap(); // Number depends on chip, etc.
     pwm.with_exported(|| {
-        pwm.enable(true).unwrap();
-        pwm.set_period_ns(20_000_000).unwrap();   // 20ms period (50 Hz)
-        loop {
-            servo_increase_to_max(&pwm, 1000, 20).unwrap();
-            servo_decrease_to_minimum(&pwm, 1000, 20).unwrap();
-        }
+        move_servo(&pwm, min_position, max_position)
     })
     .unwrap();
 }
