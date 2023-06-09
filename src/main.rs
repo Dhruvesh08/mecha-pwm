5bdd1eaeaf4a9d08e462b86cd762a55ef7ff7e5e
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 16:53:46 +0530

    mecha-pwm

--- src/main.rs
+++ src/main.rs
@@ -35,15 +35,10 @@ fn main() {
     let period = args[1].parse::<u32>().unwrap();
     let duty_cycle = args[2].parse::<u32>().unwrap();
     let pwm = Pwm::new(BB_PWM_CHIP, BB_PWM_NUMBER).unwrap(); // number depends on chip, etc.
-
-    pwm.with_exported(|| {
-        Ok({
-            pwm.enable(true).unwrap();
-            pwm.set_period_ns(period).unwrap();
-            pwm.set_duty_cycle_ns(duty_cycle).unwrap();
-            pwm.enable(false).unwrap();
-            pwm.unexport().unwrap();
-        })
-    })
-    .unwrap();
+    pwm.export().unwrap();
+    pwm.enable(true).unwrap();
+    pwm.set_period_ns(period).unwrap();
+    pwm.set_duty_cycle_ns(duty_cycle).unwrap();
+    pwm.enable(false).unwrap();
+    pwm.unexport().unwrap();
 }
