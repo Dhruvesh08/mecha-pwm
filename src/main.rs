d19d0a06dfed6e6c0e2cf31d55178f53d702ec6e
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 18:47:44 +0530

    mecha-pwm-working-v2.4

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 extern crate sysfs_pwm;
-use std::env;
 use sysfs_pwm::{Pwm, Result};
 
-// PIN: EHRPWM0A (P9_22)
-const SERVO_PWM_CHIP: u32 = 0;
-const SERVO_PWM_PIN: u32 = 0;
+// PIN: GPIO4 (P9_33)
+const LED_PWM_CHIP: u32 = 3;
+const LED_PWM_PIN: u32 = 1;
 
-fn move_servo(pwm: &Pwm, period_ns: u32, duty_cycle_ns: u32) -> Result<()> {
+fn blink_led(pwm: &Pwm, period_ns: u32, duty_cycle_ns: u32, num_cycles: u32) -> Result<()> {
     pwm.set_period_ns(period_ns)?;
     pwm.set_duty_cycle_ns(duty_cycle_ns)?;
     pwm.enable(true)?;
 
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    for _ in 0..num_cycles {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        pwm.enable(false)?;
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        pwm.enable(true)?;
+    }
 
     pwm.enable(false)?;
     pwm.set_duty_cycle_ns(0)?;
@@ -20,18 +24,9 @@ fn move_servo(pwm: &Pwm, period_ns: u32, duty_cycle_ns: u32) -> Result<()> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Usage: {} <period_ns> <duty_cycle_ns>", args[0]);
-        return;
-    }
-
-    let period_ns: u32 = args[1].parse().unwrap_or(20_000);
-    let duty_cycle_ns: u32 = args[2].parse().unwrap_or(1_000);
-
-    let pwm = Pwm::new(SERVO_PWM_CHIP, SERVO_PWM_PIN).unwrap(); // Number depends on chip, etc.
+    let pwm = Pwm::new(LED_PWM_CHIP, LED_PWM_PIN).unwrap(); // Number depends on chip, etc.
     pwm.with_exported(|| {
-        move_servo(&pwm, period_ns, duty_cycle_ns)
+        blink_led(&pwm, 1_000_000, 500_000, 5) // Blink 5 times with 1-second period and 50% duty cycle
     })
     .unwrap();
 
