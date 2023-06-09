d9fe4d16d669bf4ba795a511ffa40dd5975d387e
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 18:57:38 +0530

    mecha-pwm-working-v2.6

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,29 @@ extern crate sysfs_pwm;
 use std::env;
 use sysfs_pwm::{Pwm, Result};
 
-// PIN: GPIO4 (P9_33)
-const LED_PWM_CHIP: u32 = 0;
-const LED_PWM_PIN: u32 = 0;
-
-fn blink_led(pwm: &Pwm, period_ns: u32, duty_cycle_ns: u32, num_cycles: u32) -> Result<()> {
-    pwm.set_period_ns(period_ns)?;
-    pwm.set_duty_cycle_ns(duty_cycle_ns)?;
-    pwm.enable(true)?;
-
-    for _ in 0..num_cycles {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        pwm.enable(false)?;
-        std::thread::sleep(std::time::Duration::from_secs(1));
+fn blink_pwm(period_ns: u32, duty_cycle_ns: u32) -> Result<()> {
+    let pwm = Pwm::new(0, 0)?;
+    pwm.with_exported(|| {
         pwm.enable(true)?;
-    }
-
-    pwm.enable(false)?;
-    pwm.set_duty_cycle_ns(0)?;
+        pwm.set_period_ns(period_ns)?;
+        pwm.set_duty_cycle_ns(duty_cycle_ns)?;
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        pwm.enable(false)?;
+        Ok(())
+    })?;
 
     Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-        println!("Usage: ./blink_led <period_ns> <duty_cycle_ns> <num_cycles>");
+    if args.len() < 3 {
+        println!("Period <period_ns>, Cycle  <duty_cycle_ns>");
         return;
     }
 
     let period_ns = args[1].parse::<u32>().unwrap();
     let duty_cycle_ns = args[2].parse::<u32>().unwrap();
-    let num_cycles = args[3].parse::<u32>().unwrap();
 
-    let pwm = Pwm::new(LED_PWM_CHIP, LED_PWM_PIN).unwrap(); // Number depends on chip, etc.
-    pwm.with_exported(|| {
-        blink_led(&pwm, period_ns, duty_cycle_ns, num_cycles)
-    })
-    .unwrap();
-
-    pwm.unexport().unwrap();
+    blink_pwm(period_ns, duty_cycle_ns).unwrap();
 }
