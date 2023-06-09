3ca47d45b82d42506e79dbf249e93d4a2c7f4d0b
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-06-09 18:03:04 +0530

    mecha-pwm-working-v1

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,6 @@ use sysfs_pwm::{Pwm, Result};
 const BB_PWM_CHIP: u32 = 0;
 const BB_PWM_NUMBER: u32 = 0;
 
-fn pwm_increase_to_max(pwm: &Pwm, duration_ms: u32, update_period_ms: u32) -> Result<()> {
-    let step: f32 = duration_ms as f32 / update_period_ms as f32;
-    let mut duty_cycle = 0.0;
-    let period_ns: u32 = pwm.get_period_ns()?;
-    while duty_cycle < 1.0 {
-        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
-        duty_cycle += step;
-    }
-    pwm.set_duty_cycle_ns(period_ns)
-}
-
-fn pwm_decrease_to_minimum(pwm: &Pwm, duration_ms: u32, update_period_ms: u32) -> Result<()> {
-    let step: f32 = duration_ms as f32 / update_period_ms as f32;
-    let mut duty_cycle = 1.0;
-    let period_ns: u32 = pwm.get_period_ns()?;
-    while duty_cycle > 0.0 {
-        pwm.set_duty_cycle_ns((duty_cycle * period_ns as f32) as u32)?;
-        duty_cycle -= step;
-    }
-    pwm.set_duty_cycle_ns(0)
-}
-
 /// Make an LED "breathe" by increasing and
 /// decreasing the brightness
 fn main() {
@@ -42,8 +20,8 @@ fn main() {
             pwm.set_duty_cycle_ns(duty_cycle).unwrap();
             sleep(Duration::from_millis(3000));
             pwm.enable(false).unwrap();
-            pwm.unexport().unwrap();
         })
     })
     .unwrap();
+    pwm.unexport().unwrap();
 }
